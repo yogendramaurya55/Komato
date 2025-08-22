df63c02c9c540848488024a68910c8216afcf2c8
yogendramaurya55 <yogendramaurya9083@gmail.com>
2025-08-23 00:40:28 +0530

    inital commit

---
+++ app/src/main/java/com/example/komato/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.example.komato
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
---
+++ app/src/main/java/com/example/komato/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.komato
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.komato.ui.theme.KomatoTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KomatoTheme {
+
+            }
+        }
+    }
+}
+
